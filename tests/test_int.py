e2ea861d95109245a076ce21aa869dc859005a50
abaseen-popal <abaseen.p@gmail.com>
2021-01-03 15:25:16 +0000

    Adding the test files for jenkins to automate testing

---
+++ tests/test_int.py
@@ -0,0 +1,136 @@
+import unittest
+import time
+from flask import url_for
+from urllib.request import urlopen
+
+from flask_testing import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from application import app, db
+from application.models import Exercises,Locations,Location_exercises
+
+class TestBase(LiveServerTestCase):
+
+    def create_app(self):
+        app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:password@35.246.59.84/exercise_db"
+        app.config['SECRET_KEY'] = "dfshbdfbsidfbksbnf"
+        return app
+
+    def setUp(self):
+        """Setup the test driver and create test users"""
+        print("--------------------------NEXT-TEST----------------------------------------------")
+        chrome_options = Options()
+        chrome_options.binary_location = "/usr/bin/chromium-browser"
+        chrome_options.add_argument("--headless")
+        self.driver = webdriver.Chrome(executable_path="/home/Abase/chromedriver", chrome_options=chrome_options)
+        self.driver.get("http://localhost:5000")
+        db.session.commit()
+        db.drop_all()
+        db.create_all()
+
+    def tearDown(self):
+        self.driver.quit()
+        print("--------------------------END-OF-TEST----------------------------------------------\n\n\n-------------------------UNIT-AND-SELENIUM-TESTS----------------------------------------------")
+
+    def test_server_is_up_and_running(self):
+        response = urlopen("http://localhost:5000")
+        self.assertEqual(response.code, 200)
+
+class TestAddExercise(TestBase):
+
+    def test_add_exercise(self):
+
+
+        # Click create exercise link
+        self.driver.find_element_by_xpath("/html/body/a[2]").click()
+        time.sleep(1)
+
+        # Fill in exercise form
+        self.driver.find_element_by_xpath('//*[@id="exercise_name"]').send_keys('int testing name')
+        self.driver.find_element_by_xpath('//*[@id="description"]').send_keys('int testing description')
+        self.driver.find_element_by_xpath('//*[@id="sets"]').send_keys('int testing sets')
+        self.driver.find_element_by_xpath('//*[@id="reps"]').send_keys('int testing reps')
+        self.driver.find_element_by_xpath('//*[@id="submit"]').click()
+        time.sleep(1)
+
+        # Assert that browser redirects to home page
+        assert url_for('home') in self.driver.current_url
+        assert Exercises.query.filter_by(id=1).first().exercise_name == 'int testing name'
+
+    def test_add_location(self):
+        # Click create location link
+        self.driver.find_element_by_xpath("/html/body/a[3]").click()
+        time.sleep(1)
+
+        # Fill in location form
+        self.driver.find_element_by_xpath('//*[@id="location_name"]').send_keys('int testing location name')
+        self.driver.find_element_by_xpath('//*[@id="description"]').send_keys('int testing description')
+        self.driver.find_element_by_xpath('//*[@id="address"]').send_keys('int testing address')
+        self.driver.find_element_by_xpath('//*[@id="submit"]').click()
+        time.sleep(1)
+
+        # Assert that browser redirects to home page
+        assert url_for('home') in self.driver.current_url
+
+    # def test_Update_location(self):
+
+
+    #     # Click create location link
+    #     self.driver.find_element_by_xpath("/html/body/div[2]/form[1]/input").click()
+    #     time.sleep(1)
+
+    #     # Fill in location form
+    #     self.driver.find_element_by_xpath('//*[@id="location_name"]').send_keys('int testing location update')
+    #     self.driver.find_element_by_xpath('//*[@id="description"]').send_keys('int testing description update')
+    #     self.driver.find_element_by_xpath('//*[@id="address"]').send_keys('int testing address update')
+    #     self.driver.find_element_by_xpath('//*[@id="submit"]').click()
+    #     time.sleep(1)
+
+    #     # Assert that browser redirects to home page
+    #     assert url_for('home') in self.driver.current_url
+    #     assert Locations.query.filter_by(id=1).first().location_name == 'int testing location update'
+
+    # def test_update_exercise(self):
+    #     # Click create exercise link
+    #     self.driver.find_element_by_xpath("/html/body/div[1]/form[1]/input").click()
+    #     time.sleep(1)
+
+    #     # Fill in exercise form
+    #     self.driver.find_element_by_xpath('//*[@id="description"]').send_keys('int testing update description')
+    #     self.driver.find_element_by_xpath('//*[@id="sets"]').send_keys('int testing update sets')
+    #     self.driver.find_element_by_xpath('//*[@id="reps"]').send_keys('int testing update reps')
+    #     self.driver.find_element_by_xpath('//*[@id="submit"]').click()
+    #     time.sleep(1)
+
+    #     # Assert that browser redirects to home page
+    #     assert url_for('home') in self.driver.current_url
+
+    # def test_add_loc_exercise(self):
+    #     # Click create exercise link
+    #     self.driver.find_element_by_xpath("/html/body/div[1]/form[2]/input").click()
+    #     time.sleep(1)
+
+    #     # Fill in exercise form
+    #     self.driver.find_element_by_xpath('//*[@id="location_name"]').send_keys('int testing adding location to exercise')
+    #     self.driver.find_element_by_xpath('//*[@id="submit"]').click()
+    #     time.sleep(1)
+
+    #     # Assert that browser redirects to home page
+    #     assert url_for('view_loc_exercise') in self.driver.current_url
+
+    # def test_add_exercise_loc(self):
+    #     # Click create exercise link
+    #     self.driver.find_element_by_xpath("/html/body/div[2]/form[3]/input").click()
+    #     time.sleep(1)
+
+    #     # Fill in exercise form
+    #     self.driver.find_element_by_xpath('//*[@id="exercise_name"]').send_keys('int testing adding exercise to location')
+    #     self.driver.find_element_by_xpath('//*[@id="submit"]').click()
+    #     time.sleep(1)
+
+    #     # Assert that browser redirects to home page
+    #     assert url_for('view_location_exercise') in self.driver.current_url
+
+
+if __name__ == '__main__':
+    unittest.main(port=5000)
---
+++ tests/test_unit.py
@@ -0,0 +1,158 @@
+import unittest
+from flask import url_for
+from flask_testing import TestCase
+
+from application import app, db
+from application.models import Exercises,Locations,Location_exercises
+
+
+class TestBase(TestCase):
+    def create_app(self):
+        app.config.update(
+            SQLALCHEMY_DATABASE_URI="mysql+pymysql://root:password@35.246.59.84/exercise_db",
+            SECRET_KEY="SECRET_KEY",
+            DEBUG=True
+        )
+        return app
+
+    def setUp(self):
+        db.create_all()
+        test_exercise = Exercises(exercise_name="Test Jumping",description="Test up and down",
+        sets="3",reps="12") 
+        test_location = Locations(location_name="Test Park",description="Test description",
+        address="Test address")
+        test_location_exercise= Location_exercises(exercise_id=1,location_id=1)
+        db.session.add(test_exercise)
+        db.session.add(test_location)
+        db.session.add(test_location_exercise)
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+class TestViews(TestBase):
+    def test_home_get(self):
+        response = self.client.get(url_for('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_get(self):
+        response = self.client.get(url_for('create'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_exe_location_get(self):
+        response = self.client.get(url_for('add_exe_location',id=1),follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_update_get(self):
+        response = self.client.get(url_for('update',id=1))
+        self.assertEqual(response.status_code,200)
+
+    def test_update_location_get(self):
+        response = self.client.get(url_for('update_location',id=1),follow_redirects=True)
+        self.assertEqual(response.status_code,200)
+
+    def test_add_location_exercise_get(self):
+        response = self.client.get(url_for('add_location_exercise',id=1),follow_redirects=True)
+        self.assertEqual(response.status_code,200)
+
+    def test_view_location_exercise_get(self):
+        response = self.client.get(url_for('view_location_exercise',id=1))
+        self.assertEqual(response.status_code,200)
+
+    def test_add_loc_exercise_get(self):
+        response = self.client.get(url_for('add_loc_exercise',id=1),follow_redirects=True)
+        self.assertEqual(response.status_code,200)
+
+    def test_view_loc_exercise_get(self):
+        response = self.client.get(url_for('view_loc_exercise',id=1),follow_redirects=True)
+        self.assertEqual(response.status_code,200)
+
+    def test_delete_get(self):
+        response = self.client.get(url_for('delete',id=1),follow_redirects=True)
+        self.assertEqual(response.status_code,200)
+
+    def test_delete_location_get(self):
+        response = self.client.get(url_for('delete_location',id=1),follow_redirects=True)
+        self.assertEqual(response.status_code,200)
+
+class TestRead(TestBase):
+    def test_read_exercises(self):
+        response = self.client.get(url_for("home"))
+        self.assertIn(b"Test Jumping", response.data)
+
+class TestReadLocation(TestBase):
+    def test_read_location(self):
+        response = self.client.get(url_for("home"))
+        self.assertIn(b"Test Park", response.data)
+
+class TestCreate(TestBase):
+    def test_create_exercise(self):
+        response = self.client.post(
+            url_for("create"),
+            data=dict(exercise_name="Create a new exercise",
+            description="Test description",sets="3",
+            reps="12"),
+            follow_redirects=True
+        )
+        self.assertIn(b"Create a new exercise", response.data)
+
+class TestCreateLocation(TestBase):
+    def test_create_location(self):
+        response = self.client.post(
+            url_for("add_exe_location"),
+            data=dict(location_name="Create a new location",
+            description="Test description",
+            address="Test address"),
+            follow_redirects=True
+        )
+        self.assertIn(b"Create a new location", response.data)
+
+class TestCreateLocationExercise(TestBase):
+    def test_create_location_exercise(self):
+        response = self.client.post(
+            url_for("add_location_exercise",id = 1),
+            data=dict(exercise_name="Test Jumping"),
+            follow_redirects=True
+        )
+        self.assertIn(b"Test Jumping", response.data)
+
+class TestCreateExerciseLocation(TestBase):
+    def test_create_exercise_location(self):
+        response = self.client.post(
+            url_for("add_loc_exercise",id = 1),
+            data=dict(location_name="Test Park"),
+            follow_redirects=True
+        )
+        self.assertIn(b"Test Park", response.data)
+
+class TestUpdate(TestBase):
+    def test_update_exercise(self):
+        response = self.client.post(
+            url_for("update", id=1),
+            data=dict(exercise_name="Update an exercise",
+            description="Test Update description",
+            sets="3", reps="12"),
+            follow_redirects=True
+        )
+        self.assertIn(b"Test Update description", response.data)
+
+class TestUpdateLocation(TestBase):
+    def test_update_location(self):
+        response = self.client.post(
+            url_for("update_location", id=1),
+            data=dict(exercise_name="Update a location",
+            description="Test description",
+            address="Test Address"),
+            follow_redirects=True
+        )
+        self.assertIn(b"Test description", response.data)
+
+class TestDelete(TestBase):
+    def test_delete_exercise(self):
+        response = self.client.get(
+            url_for("delete", id=1),
+            follow_redirects=True
+        )
+        self.assertNotIn(b"Test Jumping", response.data)
